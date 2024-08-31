f4c9766d293149aff5e9884f213eb8efccef7efc
Elizeu Medeiros <elizeumedeiros@gmail.com>
2024-08-31 16:41:09 -0300

    exibir perfil

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+  data_aniversario: string,
+  name?: string,
+  email?: string,
+  sexo?: 'm' | 'f'
+  // Outros campos conforme necessário
+}
+
+
--- src/app/pages/edit-profile/edit-profile.page.ts
+++ src/app/pages/edit-profile/edit-profile.page.ts
@@ -1,5 +1,7 @@
+import { TokenService } from './../../services/token.service';
 
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user.model';
 import { UtilService } from 'src/app/services/util.service';
 
 @Component({
@@ -9,11 +11,22 @@ import { UtilService } from 'src/app/services/util.service';
 })
 export class EditProfilePage implements OnInit {
 
+  user: User;
+
   constructor(
-    public util: UtilService
+    public util: UtilService,
+    private tokenService: TokenService
   ) { }
 
   ngOnInit() {
+    this.getUserData();
+  }
+
+  getUserData() {
+    const userData = this.tokenService.getUser();
+    if (userData) {
+      this.user = userData as User; // Garanta que userData é do tipo User
+    }
   }
 
   onBack() {
